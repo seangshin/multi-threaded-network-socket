4438092a4c78705c8db36fe04c5c88b7a5fee4ee
Sean Shin <shin.seang@gmail.com>
2023-10-23 23:19:17 -0400

    MSGGET function added; global variables added; initialization steps added to main

--- multiThreadServer.c
+++ multiThreadServer.c
@@ -17,123 +17,183 @@
 
 using namespace std;
 
-#define PORT 5432  // port we're listening on
+#define PORT 2477  // port we're listening on
 #define MAX_LINE 256
 
+//
 fd_set master;   // master file descriptor list
 int listener;    // listening socket descriptor
 int fdmax;
 
+//define structure for users
+struct Users {
+  char username[20];
+  char password[20];
+};
+struct Users users[4];
+
+// *********************** Initialization Steps ******************************** //
+  char status200[] = "200 OK\n";
+  char status300[] = "300 message format error\n";
+  char status401[] = "401 You are not currently logged in, login first.\n";
+  char status402[] = "402 User not allowed to execute this command.\n";
+  char status410[] = "410 Wrong UserID or Password\n";
+  char status411[] = "411 MSGSTORE Full.\n";
+  char* msgs[20];
+  bool loginStatus = false;
+  bool root = false;
+  char newMessageBuffer[MAX_LINE]; // Buffer used to store char input for MSGSTORE
+  int messageLen; // Store number of bytes from recv
+  int msgGetCount = 0; 
+  int nextAvailableSlot = 5;
+  
+// *************************************************************************** //
+
 // the child thread
 void *ChildThread(void *newfd) {
-    char buf[MAX_LINE];
-    int nbytes;
-    int i, j;
-    int childSocket = (long) newfd;
+  char buf[MAX_LINE];
+  int nbytes;
+  int i, j;
+  int childSocket = (long) newfd;
 
-    while(1) {
-        // handle data from a client
-        if ((nbytes = recv(childSocket, buf, sizeof(buf), 0)) <= 0) {
-            // got error or connection closed by client
-            if (nbytes == 0) {
-                // connection closed
-                cout << "multiThreadServer: socket " << childSocket <<" hung up" << endl;
-            } else {
-                perror("recv");
-            }
-            close(childSocket); // bye!
-            FD_CLR(childSocket, &master); // remove from master set
-            pthread_exit(0);
-        } else {
-            // we got some data from a client
-            cout << buf;
-            for(j = 0; j <= fdmax; j++) {
-                // send to everyone!
-                if (FD_ISSET(j, &master)) {
-                    // except the listener and ourselves
-                    if (j != listener && j != childSocket) {
-                        if (send(j, buf, nbytes, 0) == -1) {
-                            perror("send");
-                        }
-                    }
-                }
-            }
+//
+
+  while(1) {
+    // handle data from a client
+    if ((nbytes = recv(childSocket, buf, sizeof(buf), 0)) <= 0) {
+      // got error or connection closed by client
+      if (nbytes == 0) {
+        // connection closed
+        cout << "multiThreadServer: socket " << childSocket <<" hung up" << endl;
+      } else {
+        perror("recv");
+      }
+      close(childSocket); // bye!
+      FD_CLR(childSocket, &master); // remove from master set
+      pthread_exit(0);
+    } else {
+      
+      // we got some data from a client
+      cout << buf;
+
+      //******************************   MSGGET *************************************** //
+      if (strcmp(buf, "MSGGET\n") == 0) { //MSGGET
+        cout << "in MSGGET" << endl;
+        if (msgGetCount >= nextAvailableSlot) {
+          msgGetCount = 0; // Reset back to the first message
         }
+        send (childSocket, status200, strlen(status200), 0); //send status successful
+        send (childSocket, msgs[msgGetCount], strlen(msgs[msgGetCount]) + 1, 0); // send message in queue
+        msgGetCount++; // point to next message
+      }
+
+      for(j = 0; j <= fdmax; j++) {
+        // send to everyone!
+        if (FD_ISSET(j, &master)) {
+          // except the listener and ourselves
+          if (j != listener && j != childSocket) {
+            if (send(j, buf, nbytes, 0) == -1) {
+              perror("send");
+            }
+          }
+        }
+      }
     }
+  }
 }
 
 
-int main(void)
-{
-    struct sockaddr_in myaddr;     // server address
-    struct sockaddr_in remoteaddr; // client address
-    int newfd;        // newly accept()ed socket descriptor
-    int yes=1;        // for setsockopt() SO_REUSEADDR, below
-    socklen_t addrlen;
+int main(void) {
+  struct sockaddr_in myaddr;     // server address
+  struct sockaddr_in remoteaddr; // client address
+  int newfd;        // newly accept()ed socket descriptor
+  int yes=1;        // for setsockopt() SO_REUSEADDR, below
+  socklen_t addrlen;
 
-    pthread_t cThread;
+  pthread_t cThread;
 
-    FD_ZERO(&master);    // clear the master and temp sets
+  FD_ZERO(&master);    // clear the master and temp sets
 
-    // get the listener
-    if ((listener = socket(AF_INET, SOCK_STREAM, 0)) == -1) {
-        perror("socket");
+  // *********************** Initialization Steps ******************************** //
+  /* Message of the day - hardcode */
+    for(int i = 0; i < 20; i++) {
+      msgs[i] = (char *)malloc(50 * sizeof(char)); // Allocate memory for each string
+    }
+    strcpy(msgs[0], "Message 1\n");
+    strcpy(msgs[1], "Message 2\n");
+    strcpy(msgs[2], "Message 3\n");
+    strcpy(msgs[3], "Message 4\n");
+    strcpy(msgs[4], "Message 5\n");
+
+    /* Users - hardcode */
+    strcpy(users[0].username, "root");
+    strcpy(users[0].password, "root01");
+    strcpy(users[1].username, "john");
+    strcpy(users[1].password, "john01");
+    strcpy(users[2].username, "david");
+    strcpy(users[2].password, "david01");
+    strcpy(users[3].username, "mary");
+    strcpy(users[3].password, "mary01");
+
+    // *************************************************************************** //
+
+  // get the listener
+  if ((listener = socket(AF_INET, SOCK_STREAM, 0)) == -1) {
+    perror("socket");
+    exit(1);
+  }
+
+  // lose the pesky "address already in use" error message
+  if (setsockopt(listener, SOL_SOCKET, SO_REUSEADDR, &yes, sizeof(int)) == -1) {
+    perror("setsockopt");
+    exit(1);
+  }
+
+  // bind
+  myaddr.sin_family = AF_INET;
+  myaddr.sin_addr.s_addr = INADDR_ANY;
+  myaddr.sin_port = htons(PORT);
+  memset(&(myaddr.sin_zero), '\0', 8);
+  if (bind(listener, (struct sockaddr *)&myaddr, sizeof(myaddr)) == -1) {
+    perror("bind");
+    exit(1);
+  }
+
+  // listen
+  if (listen(listener, 10) == -1) {
+    perror("listen");
+    exit(1);
+  }
+
+  // add the listener to the master set
+  FD_SET(listener, &master);
+
+  // keep track of the biggest file descriptor
+  fdmax = listener; // so far, it's this one
+
+  addrlen = sizeof(remoteaddr);
+
+  // main loop
+  for(;;) {
+    // handle new connections
+    if ((newfd = accept(listener, (struct sockaddr *)&remoteaddr, &addrlen)) == -1) {
+      perror("accept");
+      exit(1);
+    } else {
+      FD_SET(newfd, &master); // add to master set
+      cout << "multiThreadServer: new connection from " << inet_ntoa(remoteaddr.sin_addr) << " socket " << newfd << endl;
+
+      if (newfd > fdmax) {    // keep track of the maximum
+        fdmax = newfd;
+      }
+
+      if (pthread_create(&cThread, NULL, ChildThread, (void *)(intptr_t)newfd) <0) {
+        perror("pthread_create");
         exit(1);
+      }
     }
 
-    // lose the pesky "address already in use" error message
-    if (setsockopt(listener, SOL_SOCKET, SO_REUSEADDR, &yes, sizeof(int)) == -1) {
-        perror("setsockopt");
-        exit(1);
-    }
-
-    // bind
-    myaddr.sin_family = AF_INET;
-    myaddr.sin_addr.s_addr = INADDR_ANY;
-    myaddr.sin_port = htons(PORT);
-    memset(&(myaddr.sin_zero), '\0', 8);
-    if (bind(listener, (struct sockaddr *)&myaddr, sizeof(myaddr)) == -1) {
-        perror("bind");
-        exit(1);
-    }
-
-    // listen
-    if (listen(listener, 10) == -1) {
-        perror("listen");
-        exit(1);
-    }
-
-    // add the listener to the master set
-    FD_SET(listener, &master);
-
-    // keep track of the biggest file descriptor
-    fdmax = listener; // so far, it's this one
-
-    addrlen = sizeof(remoteaddr);
-
-    // main loop
-    for(;;) {
-        // handle new connections
-        if ((newfd = accept(listener, (struct sockaddr *)&remoteaddr, &addrlen)) == -1) {
-            perror("accept");
-	        exit(1);
-        } else {
-            FD_SET(newfd, &master); // add to master set
-            cout << "multiThreadServer: new connection from "
-		 		 << inet_ntoa(remoteaddr.sin_addr)
-                 << " socket " << newfd << endl;
-
-            if (newfd > fdmax) {    // keep track of the maximum
-                fdmax = newfd;
-            }
-
-	    if (pthread_create(&cThread, NULL, ChildThread, (void *)(intptr_t)newfd) <0) {
-                perror("pthread_create");
-                exit(1);
-            }
-        }
-
-    }
-    return 0;
+  }
+  return 0;
 }
 
